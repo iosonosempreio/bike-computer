1431deb974cad5eb7e1d13f48f32f7c81ef2ec0d
iosonosempreio <iosonosempreio@users.noreply.github.com>
2021-08-04 23:48:32 +0200

    jsx express engine

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 /* Simple Express.js Server */
 const express = require("express")
 const app = express()
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 const port = 3000
 app.get("/", (req, res) => res.send("Hello World!"))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
